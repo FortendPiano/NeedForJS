ad4b803801e341c82cae09be14b5744ab780df58
Andrey <andrey_y4in_1297@bk.ru>
2019-12-15 02:36:00 +0300

    Первый урок

---
+++ main.js
@@ -0,0 +1,46 @@
+const score = document.querySelector(".score"),
+    start = document.querySelector(".start"),
+    gameArea = document.querySelector(".gameArea"),
+    car = document.createElement("div");
+car.classList.add('car');
+const keys = {
+    ArrowUp: false,
+    ArrowDown: false,
+    ArrowLeft: false,
+    ArrowRight: false
+};
+const setting = {
+    start: false,
+    score: 0,
+    speed: 3
+};
+
+start.addEventListener('click', startGame);
+document.addEventListener('keydown', startRun);
+document.addEventListener('keyup', stopRun);
+
+function startGame() {
+    start.classList.add('hide');
+    setting.start = true;
+    gameArea.appendChild(car);
+    requestAnimationFrame(playGame);
+}
+
+function playGame() {
+    console.log("play");
+    if(setting.start) {
+        requestAnimationFrame(playGame);   
+    }
+    
+}
+
+function startRun(event) {
+    event.preventDefault();
+    keys[event.key] = true;
+    console.log(event.key);
+}
+
+function stopRun(event) {
+    event.preventDefault();
+    keys[event.key] = false;
+}
