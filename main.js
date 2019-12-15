9adba501298d38a9c09bf998475b041c34c49655
Andrey <andrey_y4in_1297@bk.ru>
2019-12-15 08:17:58 +0300

    Третий урок

--- main.js
+++ main.js
@@ -26,7 +26,8 @@ document.addEventListener('keyup', stopRun);
 
 function startGame() {
     start.classList.add('hide');
-
+    gameArea.innerHTML = '';
+    
     for(let i = 0; i < getQuantityElements(100) ; i++) {
         const line = document.createElement('div');
         line.classList.add('line');
@@ -45,8 +46,12 @@ function startGame() {
         gameArea.appendChild(enemy);
     }
 
+    setting.score = 0;
     setting.start = true;
     gameArea.appendChild(car);
+    car.style.left = gameArea.offsetWidth / 2 - car.offsetWidth / 2;
+    car.style.top = 'auto';
+    car.style.bottom = '10px';
     setting.x = car.offsetLeft;
     setting.y = car.offsetTop;
     requestAnimationFrame(playGame);
@@ -54,6 +59,8 @@ function startGame() {
 
 function playGame() {
     if(setting.start) {
+        setting.score +=setting.speed;
+        score.innerHTML = "Score<br>" + setting.score;
         moveRoad();
         moveEnemy();
         if(keys.ArrowLeft && setting.x > 0) {
@@ -102,6 +109,17 @@ function moveRoad() {
 function moveEnemy() {
     let enemy = document.querySelectorAll('.enemy');
     enemy.forEach(function(item) {
+        let carRect = car.getBoundingClientRect(),
+            enemyRect = item.getBoundingClientRect();
+
+        if(carRect.top <= enemyRect.bottom && 
+        carRect.right >= enemyRect.left &&
+        carRect.left <= enemyRect.right && 
+        carRect.bottom >= enemyRect.top) {
+            setting.start = false;
+            start.classList.remove('hide');
+            score.style.top = start.offsetHeight;
+        }
         item.y += setting.speed / 2;
         item.style.top = item.y + 'px';
         if(item.y >= document.documentElement.clientHeight) {
